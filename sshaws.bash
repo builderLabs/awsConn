d07587ca43b0d0f39138db2a687de7e76fc0c1fc
builderLabs <ozanakcin@gmail.com>
2017-11-06 15:42:04 -0500

    refactor: Reworked bash wrapper
    Redrafted bash wrapper for invocation of underlying Perl script
    awsConn.pl to allow command line arguments for server and
    instance arguments.  With use of OPTIND, the input options array
    is now emptied/re-initialized with each invocation in a given
    session, allowing for the script to be conveniently sourced as
    part of a bash profile (for utility functions, and the like).

--- sshaws.bash
+++ sshaws.bash
@@ -1,21 +1,70 @@
 #====================================================================
-#
-#  Filename:        sshaws.bash
-#  Author:          Ozan Akcin
-#  Created:         20161202
-#  Purpose:
-#  Notes:
-#
+function sshawsUsage() {
+    echo
+    echo "     $0 -c <cmd> [ OPTIONAL: -i <inst> ]"
+    echo "         cmd:      command [ <start|stop|connect|describe|status> | DEFAULT: connect ]"
+    echo "         inst:     instance [ <dev|prod> | DEFAULT: dev ]"
+    echo
+    return
+}
 #====================================================================
 
-#!/bin/bash
 
+#====================================================================
+function sshaws() {
 
-awsConnMgr=${HOME}/fsbase/releases/fsprod/src/util/awsconn/awsConn.pl
+# empty input options array to purge stale args from prev sessions
+OPTIND=""
 
-task=connect
-if [[ $# > 0 ]]; then
-   task=$1
+inst=""
+cmd=""
+av_inst=""
+av_cmd=""
+
+pv_inst="dev"
+pv_cmd="connect"
+
+while getopts "i:c:h" opt
+do
+   case $opt in
+        i )  av_inst=$OPTARG;;
+        c )  av_cmd=$OPTARG;
+             if [[ $av_cmd == "stat" || $av_cmd == "status" ]]; then
+                av_cmd="describe"
+             elif [[ $av_cmd == "conn" ]]; then
+                av_cmd="connect"
+             fi;;
+        h)  sshawsUsage;
+            return;;
+        * ) sshawsUsage;
+            return;;
+   esac
+done
+
+inst=${av_inst:-$pv_inst}
+cmd=${av_cmd:-$pv_cmd}
+
+# confirm if instance stoppage
+if [ $cmd == "stop" ]; then
+   read -p "Are you sure? Confirm $cmd for $inst instance (Y/y): " -n 1 -r
+   echo
+   if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+      echo "Aborting..."
+      return
+   fi
 fi
 
-`perl -w $awsConnMgr --argv \cmd=$task`
+case $cmd in
+   "connect" ) pv_cmd_v="Connecting to ";;
+   "start"   ) pv_cmd_v="Starting ";;
+   "stop"    ) pv_cmd_v="Stopping ";;
+   * )         pv_cmd_v="Checking status for ";;
+esac
+
+
+echo $pv_cmd_v instance: $inst ...
+perl -w ${HOME}/fsbase/releases/fsprod/src/util/awsConn.pl --argv inst=$inst --argv cmd=$cmd
+
+}
+#====================================================================
+
